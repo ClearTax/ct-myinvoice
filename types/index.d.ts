8b978a566d23fa12eab6fa6f7245578ec0b3f550
Ekta34 <ekta.mehta@clear.in>
2024-10-25 16:00:13 +0530

    remove callback func and added parent origin in query params

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,6 @@
 // types/index.d.ts
 export type EnvironmentType = 'development' | 'sandbox' | 'production';
 
-export interface CallbackData {
-  statusCode: number;
-}
-
 export interface ICreateIframeOptions {
   token: string;
   width?: string;
@@ -13,7 +9,6 @@ export interface ICreateIframeOptions {
   environment?: EnvironmentType;
   title?: string;
   style?: { [key: string]: string }; // Optional inline style object
-  callbackFunc?: (data: CallbackData) => void;
 }
 
 export default function createIframe(
